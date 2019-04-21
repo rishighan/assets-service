fc8571ba1e1a1baea4c72aaeb513dc48f58bbeaf
Rishi Ghan <rishi.ghan@gmail.com>
2019-04-21 10:26:56 -0700

    First commit

---
+++ services/assets.service.js
@@ -0,0 +1,40 @@
+"use strict";
+
+module.exports = {
+	name: "assets",
+	version: 1,
+	settings: {
+
+	},
+	dependencies: [],	
+	actions: {
+	
+	},
+	events: {
+
+	},
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+
+	}
+};
