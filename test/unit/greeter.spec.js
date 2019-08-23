5b791c412f1f222dd38c22d6fee1c4187f5788b0
Rishi Ghan <hghan@apple.com>
2019-08-23 10:41:46 -0700

    Removed unit tests placeholders

--- test/unit/greeter.spec.js
+++ test/unit/greeter.spec.js
@@ -2,7 +2,7 @@
 
 const { ServiceBroker } = require("moleculer");
 const { ValidationError } = require("moleculer").Errors;
-const TestService = require("../../services/greeter.service");
+const TestService = require("../../services/assets.service");
 
 describe("Test 'greeter' service", () => {
 	let broker = new ServiceBroker({ logger: false });
@@ -11,25 +11,7 @@ describe("Test 'greeter' service", () => {
 	beforeAll(() => broker.start());
 	afterAll(() => broker.stop());
 
-	describe("Test 'greeter.hello' action", () => {
-
-		it("should return with 'Hello Moleculer'", () => {
-			expect(broker.call("greeter.hello")).resolves.toBe("Hello Moleculer");
-		});
-
-	});
-
-	describe("Test 'greeter.welcome' action", () => {
-
-		it("should return with 'Welcome'", () => {
-			expect(broker.call("greeter.welcome", { name: "Adam" })).resolves.toBe("Welcome, Adam");
-		});
-
-		it("should reject an ValidationError", () => {
-			expect(broker.call("greeter.welcome")).rejects.toBeInstanceOf(ValidationError);
-		});
-
-	});
+	
 
 });
 
