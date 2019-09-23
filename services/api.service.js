457cd2d56d5f675f6f9d4b2bf2a344c229b081e0
Rishi Ghan <rishi@Rishis-Mac-mini.local>
2019-09-23 15:14:46 -0700

    S3 upload action implemented

--- services/api.service.js
+++ services/api.service.js
@@ -1,69 +1,57 @@
 "use strict";
-const Busboy = require('busboy');
 const ApiGateway = require("moleculer-web");
-function uploader(req, res, next) {
-	console.log(req.body)
-	const busboy = new Busboy({ headers: req.headers });
-	busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
-		console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-		file.on('data', function (data) {
-			console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-		});
-		file.on('end', function () {
-			console.log('File [' + fieldname + '] Finished');
-		});
-	});
-
-	res.end(next);
-}
 
 module.exports = {
-    mixins: [ApiGateway],
-    settings: {
-        path: "/upload",
+	mixins: [ApiGateway],
+	settings: {
+		path: "/upload",
+		port: 4000,
+		// Global CORS settings for all routes
+		cors: {
+			// Configures the Access-Control-Allow-Origin CORS header.
+			origin: "*",
+			// Configures the Access-Control-Allow-Methods CORS header. 
+			methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+			// Configures the Access-Control-Allow-Headers CORS header.
+			allowedHeaders: [],
+			// Configures the Access-Control-Expose-Headers CORS header.
+			exposedHeaders: [],
+			// Configures the Access-Control-Allow-Credentials CORS header.
+			credentials: true,
+			// Configures the Access-Control-Max-Age CORS header.
+			maxAge: 3600
+		},
+		routes: [
+			{
+				path: "",
+				// You should disable body parsers
+				bodyParsers: {
+					json: false,
+					urlencoded: false
+				},
 
-        routes: [
-            {
-                path: "",
+				aliases: {
+					// File upload from HTML multipart form
+					"POST /": "multipart:v1.assets.upload",
+					// File upload from AJAX or cURL
+					"PUT /": "stream:assets.upload",
 
-                // You should disable body parsers
-                bodyParsers: {
-                    json: false,
-                    urlencoded: false
-                },
+					// File upload from HTML form and overwrite busboy config
+					"POST /multi": {
+						type: "multipart",
+						// Action level busboy config
+						busboyConfig: {
+							limits: {
+								files: 3
+							}
+						},
+						action: "v1.assets.upload"
+					}
+				},
 
-                aliases: {
-                    // File upload from HTML multipart form
-                    "POST /": "multipart:assets.upload",
-                    
-                    // File upload from AJAX or cURL
-                    "PUT /": "stream:assets.upload",
 
-                    // File upload from HTML form and overwrite busboy config
-                    "POST /multi": {
-                        type: "multipart",
-                        // Action level busboy config
-                        busboyConfig: {
-                            limits: {
-                                files: 3
-                            }
-                        },
-                        action: "assets.upload"
-                    }
-                },
-
-                // Route level busboy config.
-                // More info: https://github.com/mscdex/busboy#busboy-methods
-                busboyConfig: {
-                    limits: {
-                        files: 1
-                    }
-                    // Can be defined limit event handlers
-                    // `onPartsLimit`, `onFilesLimit` or `onFieldsLimit`
-                },
-
-                mappingPolicy: "restrict"
-            }
-        ]
-    }
+				mappingPolicy: "restrict"
+			}
+		]
+	}
 };
--- services/assets.service.js
+++ services/assets.service.js
@@ -1,11 +1,11 @@
 "use strict";
 const AWS = require('aws-sdk');
 const dotenv = require("dotenv");
-const awsCredentials = dotenv.config()
+const awsCredentials = dotenv.config();
 
 const s3 = new AWS.S3({
 	accessKeyId: awsCredentials.parsed.AWS_ACCESS_KEY_ID,
-	secretAccessKey: awsCredentials.parsed.AWS_SECRET_ACCESS_KEY, 
+	secretAccessKey: awsCredentials.parsed.AWS_SECRET_ACCESS_KEY,
 });
 
 
@@ -22,19 +22,30 @@ module.exports = {
 				keys: [],
 			},
 			params: {
-				// payload: { type: "object", optional: false } ,
+				// data: { type: "object", optional: false } ,
 
 			},
 			handler(context) {
-				console.log('PARAMS', context.params);
+				const fileStream = context.params;
 				const params = {
 					Bucket: awsCredentials.parsed.S3_BUCKET_NAME,
-					Body: context.meta,
-					Key: "key",
+					Body: fileStream,
+					Key: context.meta.filename,
 				};
 				s3.upload(params, (error, data) => {
 					console.log(error, data);
 				});
+
+			}
+		},
+		delete: {
+			params: {
+				fileID: {
+					type: "string",
+					optional: false
+				}
+			},
+			handler(context) {
 
 			}
 		}
@@ -43,14 +54,13 @@ module.exports = {
 
 	},
 	methods: {
-
 	},
 
 	/**
 	 * Service created lifecycle event handler
 	 */
 	created() {
-		
+
 		console.log("Assets service instance created");
 	},
 
